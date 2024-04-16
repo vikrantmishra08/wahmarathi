5ae7fbb6c2c73c7962d462508e45d9533957719c
Vikrant Mishra <84456926+vikrantmishra08@users.noreply.github.com>
2024-04-16 15:14:41 +0530

    Wahmarathi

---
+++ app.js
@@ -0,0 +1,50 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let items = document.querySelectorAll('.slider .list .item');
+    let thumbnails = document.querySelectorAll('.thumbnail .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    let countItem = items.length;
+    let itemActive = 0;
+    let refreshInterval;
+
+    function startAutoSlider() {
+        refreshInterval = setInterval(() => {
+            next.click();
+        }, 5000);
+        }
+
+    function stopAutoSlider() {
+        clearInterval(refreshInterval);
+        }
+
+    function showSlider() {
+        let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+    stopAutoSlider();
+    startAutoSlider();
+        }
+
+    next.onclick = function () {
+        itemActive = (itemActive + 1) % countItem;
+    showSlider();
+        };
+
+    prev.onclick = function () {
+        itemActive = (itemActive - 1 + countItem) % countItem;
+    showSlider();
+        };
+
+        thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', () => {
+            itemActive = index;
+            showSlider();
+        });
+        });
+
+    startAutoSlider();
+    });
---
+++ slider.js
@@ -0,0 +1,39 @@
+let customSlideIndex = 1;
+customShowSlides(customSlideIndex);
+
+// Automatically change slides every 2 seconds
+let customSlideInterval = setInterval(function () {
+    customPlusSlides(1);
+}, 2000);
+
+function customPlusSlides(n) {
+    clearInterval(customSlideInterval); // Clear the previous interval
+    customSlideInterval = setInterval(function () {
+        customPlusSlides(1);
+    }, 2000); // Set new interval
+    customShowSlides(customSlideIndex += n);
+}
+
+function customCurrentSlide(n) {
+    clearInterval(customSlideInterval); // Clear the previous interval
+    customSlideInterval = setInterval(function () {
+        customPlusSlides(1);
+    }, 2000); // Set new interval
+    customShowSlides(customSlideIndex = n);
+}
+
+function customShowSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("customSlides");
+    let dots = document.getElementsByClassName("custom-dot");
+    if (n > slides.length) { customSlideIndex = 1 }
+    if (n < 1) { customSlideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[customSlideIndex - 1].style.display = "block";
+    dots[customSlideIndex - 1].className += " active";
+}
